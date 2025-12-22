4b9c75c076f0cd5ec32451b5a9a18ba70e1d734f
ALiangNe <2650321653@qq.com>
2025-12-22 11:02:49 +0800

    Initial project

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8888",
+      chainId: 1337
+    },
+  },
+};
+
+export default config;
